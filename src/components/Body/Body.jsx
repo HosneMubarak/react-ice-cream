eec70d199be67122590b7a6ca7241459a390e68f
Hosne Mubarak <h.m.tasmir@gmail.com>
2021-11-27 01:06:18 +0600

    uploading

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Body.module.css';
+
+const Body = props => (
+   <div className={classes.mainBody}>
+
+        </div>
+);
+
+export default Body;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Layout = function (props) {
+    return (
+        <div>{props.children}</div>
+    );
+};
+
+export default Layout;
